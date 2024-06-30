9efa25887dea90785efa43fdcd85b0a508bd58b5
JonasLProgramador <jonas.programador01@gmail.com>
2024-06-30 17:09:12 -0300

    feat: implementing the project

---
+++ src/components/card/index.jsx
@@ -0,0 +1,13 @@
+const Card = ({ character }) => {
+    return (
+        <div className="card flex flex-col items-center justify-center p-4 bg-white bg-opacity-90 rounded-lg shadow-lg m-4 w-60">
+            <img src={character.image} alt={character.name} className="rounded-full w-32 h-32 mb-4"/>
+            <h2 className="text-xl font-bold text-gray-700 mb-2">{character.name}</h2>
+            <p className="text-gray-600">Status: {character.status}</p>
+            <p className="text-gray-600">Espécie: {character.species}</p>
+            <a href={`/character/${character.id}`} className="text-blue-500 hover:underline mt-4">Mais detalhes</a>
+        </div>
+    );
+};
+
+export default Card;
