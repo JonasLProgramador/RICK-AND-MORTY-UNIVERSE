f5bbcea7b828e68b2e2373f4240f8064988ca8f5
JonasLProgramador <jonas.programador01@gmail.com>
2024-06-30 17:37:22 -0300

    feat: implemeting the page for  random character

--- src/view/characterPage/index.jsx
+++ src/view/characterPage/index.jsx
@@ -1,9 +1,33 @@
-const CharacterPage = () => {
-    return(
-        <div>
-            <h1>Character Page</h1>
-        </div>
-    )
-}
+// src/pages/HomePage.js
+import { useEffect, useState } from 'react';
+import { fetchRandomCharacters } from '../models/fetchPersonagens';
+import Card from '../components/Card';
+import Header from '../components/Header';
 
-export default CharacterPage
+const CharacterPage = () => {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        const loadCharacters = async () => {
+            const randomCharacters = await fetchRandomCharacters();
+            setCharacters(randomCharacters);
+        };
+        loadCharacters();
+    }, []);
+
+    return (
+        <div className="flex flex-col items-center min-h-screen bg-[url('./src/assets/rick-and-morty.gif')] bg-cover">
+            <Header links={[
+                { id: 1, to: "/", text: "Home" },
+                { id: 2, to: "/history", text: "Histórico" }
+            ]} />
+            <div className="flex flex-wrap justify-center mt-10">
+                {characters.map(character => (
+                    <Card key={character.id} character={character} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default CharacterPage;
